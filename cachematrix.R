c375f5b68f160a6ece4c91a8ac88eebc6f7261b0
Alejandra <alejandra.ferguson@icloud.com>
2018-03-05 13:02:57 +0800

    Changes to makeCacheMatrix and cacheSolve for Assgnmt2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      M <- NULL
+      set <- function(y) {
+            x <<- y
+            M <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) M <<- solve
+      
+      getinverse <- function() M
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      M <- x$getinverse()
+      if(!is.null(M)) {
+            message("getting cached data")
+            return(M)
+      }
+      data <- x$get()
+      M <- solve(data, ...)
+      x$setinverse(M)
+      M
 }
